1ec7caf785a126384903a0069a211e14a961dae2
DengChristian <92216459+DengChristian@users.noreply.github.com>
2021-12-24 19:38:05 +0800

    云函数addXiaoLiang

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-// 云函数入口文件
+//添加销量
 const cloud = require('wx-server-sdk')
-
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
 })
-
-// 云函数入口函数
+// 云函数入口函数
 exports.main = async (event, context) => {
-  return await cloud.database().collection('pinglun')
-    .where({
-      goodId: event.goodId
-    }).get()
+
+  let arr = []
+  const db = cloud.database()
+  const _ = db.command
+  event.goods.forEach(item => {
+    let pro = db.collection('goods').doc(item._id)
+      .update({
+        data: {
+          num: _.inc(item.quantity)
+        }
+      })
+    arr.push(pro)
+  })
+  return await Promise.all(arr).then(res => {
+    return res
+  }).catch(res => {
+    return res
+  })
 }
